88b5a378dbda25d4c24722632fc832616f26b9cb
reutov <underwater.aviator@gmail.com>
2017-09-06 16:54:36 +0300

    Handle command "/start"

---
+++ src/api/Commands.php
@@ -0,0 +1,44 @@
+<?php 
+    require "../vendor/autoload.php";
+    
+    class Commands {
+        private $bot;
+        private $lastChatId;
+        private $geoLocation;
+      
+        private $geoKeyboard = [
+            'keyboard' => [
+                [
+                   [
+                       'text' => 'Отправить местоположение',
+                       'request_location'=>true
+                  ]
+                ] 
+                
+            ],
+            'resize_keyboard' => true,
+            'one_time_keyboard' => true
+        ];
+
+        public function __construct($bot) {
+            $this->bot = $bot;
+            
+        }
+        public function handleCommand($command) {
+            if( $command == "/start" ) {
+                $this->handleStart();
+            } else {
+            }
+        }
+       
+        private function handleStart() {
+            $this->bot->sendMessage( "Почтовый бот\n ", $this->geoKeyboard);
+            
+        }
+       
+
+        public function setLastChatId($chatId) {
+            $this->lastChatId = $chatId;
+        }
+    }
+    ?>
--- src/index.php
+++ src/index.php
@@ -2,10 +2,21 @@
     require "../vendor/autoload.php";
 
     require "api/telegram.php";
+    require "api/Commands.php";
 
     try {
         $tl = new Telegram("https://api.telegram.org/", "bot310341855:AAGF60Bu1mHjDjjEn31ekxwJmKw-OMTBlqg/");
-        print_r($tl->getLocation());
+        $location;
+            while(1) {
+                sleep(3);
+                $command = new Commands($tl);
+                $lastMessage = $tl->getLastMessage();
+                if(!empty($lastMessage)) {
+                    $tl->confirmMessage();
+                    $command->handleCommand($lastMessage);
+                }
+                    
+            }
         
     } catch(Exception $e) {
         echo "Exception: " . $e;
