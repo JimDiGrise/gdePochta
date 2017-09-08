fd6fa1886d75bb0694942bb9e212728ac8866627
reutov <underwater.aviator@gmail.com>
2017-09-08 23:39:50 +0300

    Get list of offices by geo location

--- src/api/yandex.php
+++ src/api/yandex.php
@@ -35,5 +35,21 @@
             $response = $this->httpClient->request('GET', "https://static-maps.yandex.ru/1.x/?ll=$geo[0],$geo[1]&size=250,250&z=17&l=map&pt=$geo[0],$geo[1],pm2dgm&scale=1.0");
             file_put_contents("img.png", $response->getBody());  
         }    
+        public function getItemsByGeoLocation($geo) {
+            $response = $this->httpClient->request('GET', "https://search-maps.yandex.ru/v1/?apikey=37cc2574-03a2-444f-8f5f-caafaae8efe1&text=Отделение почты &lang=ru-RU&type=biz&results=100&ll=$geo->longitude, $geo->latitude&spn=0.015,0.015&rspn=1");
+            $responseBody = json_decode($response->getBody())->features;
+            $pochta = new Pochta();
+            foreach($responseBody as $office => $value) {
+                $responseBody[$office]->properties->CompanyMetaData->name = "Почтовое отделение #" . $pochta->getIndex($responseBody[$office]->properties->CompanyMetaData->address);
+            }
+            return array_filter($responseBody, function ($obj) {
+                static $idList = array();
+                if(in_array($obj->properties->CompanyMetaData->name, $idList)) {
+                    return false;
+                }
+                $idList []= $obj->properties->CompanyMetaData->name;
+                return true;
+            });;
+        }
     }
 ?>
